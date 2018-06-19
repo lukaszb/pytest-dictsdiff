a1b8487f494b892ef86ffd898cdc4d2c518fb63d
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2018-06-19 10:05:09 +0200

    Updated readme and bumped version

--- README.md
+++ README.md
@@ -17,6 +17,7 @@ def test_dicts(dicts_are_same):
     dict1 = {'a': 1, 'b': 2, 'c': {'d': 4, 'e': 5}}
     dict2 = {'a': 100, 'f': 2, 'c': {'d': 400, 'g': 5}}
     assert dicts_are_same(dict1, dict2)
+```
 
 ## Todos
 
--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import setup
 
 setup(
     name="pytest-dictsdiff",
-    version='0.5',
+    version='0.5.4',
     py_modules=["pytest_dictsdiff"],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author='Lukasz Balcerzak',
     author_email='lukaszbalcerzak@gmail.com',
     entry_points={"pytest11": ["dictsdiff = pytest_dictsdiff"]},
