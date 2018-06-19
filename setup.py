6c73e01d4b481eac553b2ec01989df6a153e35b1
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2018-06-19 09:51:59 +0200

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="pytest-dictsdiff",
     version='0.5',
-    packages=["pytest_dictsdiff"],
+    py_modules=["pytest_dictsdiff"],
     author='Lukasz Balcerzak',
     author_email='lukaszbalcerzak@gmail.com',
     entry_points={"pytest11": ["dictsdiff = pytest_dictsdiff"]},
